a7cd22991272c3dcb32575d5082d30f74e4e7a74
apple2peak <sunshineapple@126.com>
2016-03-16 11:26:24 +0800

    second

--- src/com/sina/tianqitong/Login.java
+++ src/com/sina/tianqitong/Login.java
@@ -1,6 +1,8 @@
 package com.sina.tianqitong;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Login {
@@ -13,10 +15,19 @@ public class Login {
 		System.out.println("×Ô¶¯»¯²âÊÔ");  
 		System.setProperty("webdriver.chrome.driver", "C://Program Files//Google//Chrome//Application//chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
-		driver.get("http://weibo.com");
-		//WebElement aboutLink = driver.findElement(By.linkText("ÕÊºÅµÇÂ¼"));
-		//aboutLink.click();
-		Thread.sleep(2000);
+		driver.get("http://mail.sina.com.cn");
+		WebElement username = driver.findElement(By.id("freename"));
+		username.click();
+		username.sendKeys("yepingping3840@sina.com");
+		WebElement password = driver.findElement(By.id("freepassword"));
+		password.click();
+		password.sendKeys("qwer1234");
+		WebElement login = driver.findElement(By.className("loginBtn"));
+		login.click();
+		Thread.sleep(3000);
+		WebElement weibo = driver.findElement(By.className("wbAllLogo"));
+		weibo.click();
+		Thread.sleep(3000);
 	   driver.close();
 	   driver.quit();
 	}
